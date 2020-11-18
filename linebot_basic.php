8c607c1c1772e46897152a0582643cee6e3287a9
Ratanapxrn <70052100+Ratanapxrn@users.noreply.github.com>
2020-11-18 15:26:14 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -19,7 +19,7 @@ foreach ($request_json['events'] as $event)
 			//$reply_message = 'Ratanaporn';   
 			
 			$reply_message = mySQL_selectAll('http://s61160191.kantit.com/json_select.php');
-                        }
+                        
                         if($test[1] == "ฉันต้องการค้นหาข้อมูลนิสิตทั้งหมด"){
 				$reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php');
 			}
