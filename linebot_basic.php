b50ee7c5b159e582405e7ea003d5a6ddd4d8d703
Ratanapxrn <70052100+Ratanapxrn@users.noreply.github.com>
2020-11-18 15:14:07 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -18,8 +18,8 @@ foreach ($request_json['events'] as $event)
 			
 			//$reply_message = 'Ratanaporn';   
 			
-			//$reply_message = mySQL_selectAll('http://s61160191.kantit.com/json_select.php');
-                          $reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php')              
+			$reply_message = mySQL_selectAll('http://s61160191.kantit.com/json_select.php');
+                                     
 		} else {
 			$reply_message = 'ฉันได้รับ '.$event['message']['type'].' ของคุณแล้ว!';
 		}
